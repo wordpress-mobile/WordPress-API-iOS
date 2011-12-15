6490e954d47038298dd3eb987459c2d8cfeadf1f
Jorge Bernal <jbernal@gmail.com>
2011-12-15 21:34:09 +0100

    Initial import

---
+++ WordPressApi/WordPressApi/WordPressApi.h
@@ -0,0 +1,13 @@
+//
+//  WordPressApi.h
+//  WordPressApi
+//
+//  Created by Jorge Bernal on 12/15/11.
+//  Copyright (c) 2011 Automattic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WordPressApi : NSObject
+
+@end
---
+++ WordPressApi/WordPressApi/WordPressApi.m
@@ -0,0 +1,23 @@
+//
+//  WordPressApi.m
+//  WordPressApi
+//
+//  Created by Jorge Bernal on 12/15/11.
+//  Copyright (c) 2011 Automattic. All rights reserved.
+//
+
+#import "WordPressApi.h"
+
+@implementation WordPressApi
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
