431d34e29e86ed7ace48248e670f00b65168e0f5
Jorge Bernal <jbernal@gmail.com>
2012-01-17 18:10:46 +0100

    Updated AFNetworking and XMLRPC to point to the new wordpress-mobile repo

---
+++ WordPressApiExample/WordPressApiExample/ComposeViewController.h
@@ -0,0 +1,13 @@
+//
+//  ComposeViewController.h
+//  WordPressApiExample
+//
+//  Created by Jorge Bernal on 1/17/12.
+//  Copyright (c) 2012 Automattic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ComposeViewController : UIViewController
+
+@end
