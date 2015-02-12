36d7df67908c19c3250e197ebad1f8ecdf1ce0e5
Diego Rey Mendez <diegoreymendez@gmail.com>
2015-02-12 14:05:24 -0300

    Removed header comments as specified in our style guide.

--- WordPressApi/WPXMLRPCRequest.h
+++ WordPressApi/WPXMLRPCRequest.h
@@ -1,11 +1,3 @@
-//
-//  AFXMLRPCRequest.h
-//  WordPress
-//
-//  Created by Jorge Bernal on 2/20/13.
-//  Copyright (c) 2013 WordPress. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 /**
--- WordPressApiExample/WordPressApiExample/PostViewController.h
+++ WordPressApiExample/WordPressApiExample/PostViewController.h
@@ -1,11 +1,3 @@
-//
-//  DetailViewController.h
-//  WordPressApiExample
-//
-//  Created by Jorge Bernal on 12/20/11.
-//  Copyright (c) 2011 Automattic. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface PostViewController : UIViewController
