0fb234c5b46a05957451a2121f85576e99dc07ce
Matt Bumgardner <matt@bumgardner.org>
2016-03-08 10:57:28 -0600

    Added 404 test

--- Example/Tests/WordPressXMLRPCApiTests.m
+++ Example/Tests/WordPressXMLRPCApiTests.m
@@ -1,5 +1,7 @@
 #import <XCTest/XCTest.h>
 #import <WordPressApi.h>
+#import <OHHTTPStubs/OHHTTPStubs.h>
+#import <OHHTTPStubs/OHHTTPStubsResponse.h>
 
 @interface WordPressXMLRPCApiTests : XCTestCase
 
@@ -15,6 +17,7 @@
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
+    [OHHTTPStubs removeAllStubs];
 }
 
 - (void)testGuessXMLRPCURLForSite {
@@ -24,6 +27,30 @@
         [expectation fulfill];
     }];
     [self waitForExpectationsWithTimeout:2 handler:nil];
+}
+
+- (void)testServerSide404Response
+{
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Call should fail with error when server returns 404"];
+    [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+        return [request.URL.host isEqualToString:@"mywordpresssite.com"];
+    } withStubResponse:^OHHTTPStubsResponse*(NSURLRequest *request) {
+        return [[OHHTTPStubsResponse responseWithData:[NSData data] statusCode:404 headers:nil]
+         responseTime:OHHTTPStubsDownloadSpeedWifi];
+    }];
+    
+    [WordPressApi signInWithURL:@"mywordpresssite.com"
+                       username:@"username"
+                       password:@"password"
+                        success:^(NSURL *xmlrpcURL) {
+                            XCTFail(@"Call to site returning a 404 should not enter success block.");
+                        } failure:^(NSError *error) {
+                            XCTAssertNotNil(error);
+                            XCTAssertTrue([error.userInfo[@"NSLocalizedDescription"] rangeOfString:@"404"].location != NSNotFound);
+                            [expectation fulfill];
+                        }];
+
+    [self waitForExpectationsWithTimeout:5 handler:nil];
 }
 
 @end
